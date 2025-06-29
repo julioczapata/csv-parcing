9316441a2a6fba1d40a6c857c2fe901a282e6143
jelpaisa <juliozapata.mba@gmail.com>
2025-06-28 20:16:16 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBXZ4UXQoh-UdyxGcHgD8bKO34NnOIWXlY",
+  authDomain: "csv-parcing.firebaseapp.com",
+  projectId: "csv-parcing",
+  storageBucket: "csv-parcing.firebasestorage.app",
+  messagingSenderId: "950554258427",
+  appId: "1:950554258427:web:f6b9abb005e6b1aae6f95e",
+  measurementId: "G-RJFSXPGWPS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
